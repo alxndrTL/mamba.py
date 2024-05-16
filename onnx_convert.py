7be9fe117d61bfe69d64e8f71e9064914457a1a9
Zeng-WCh <lewis.zeng02@gmail.com>
2024-05-16 23:30:21 +0800

    <bug fixed>: use eval model when export

--- onnx_convert.py
+++ onnx_convert.py
@@ -1,13 +1,15 @@
 import torch
 from mamba_lm_onnx import from_pretrained
 
-model = from_pretrained('state-spaces/mamba-130m').to('cuda')
+model = from_pretrained('state-spaces/mamba-370m')
+model.eval()
 
-
-# Note that this may not work 
+# Note that this may not work
 # so you may need to adjust the input shape and type
-torch.onnx.export(model, 
-                  torch.zeros(1, dtype=torch.int64, device='cuda'), 
-                  'mamba-130m.onnx', 
-                  input_names=['input'], 
-                  output_names=['output'],)
+torch.onnx.export(model,
+                  torch.zeros(1, dtype=torch.int64),
+                  'mamba-370m.onnx',
+                  input_names=['input'],
+                  output_names=['output'],
+                  opset_version=17,
+                  )
