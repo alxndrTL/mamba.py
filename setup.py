dcd6a326ad92eb2dd375e1c1ff67b48823246364
Alexandre TL <alextorresleguet@icloud.com>
2024-07-31 14:03:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mambapy',
-    version='1.1.0',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=[
         'torch',
